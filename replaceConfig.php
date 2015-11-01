e2df5f87e194ce07bb145bef6b34ac8a3f0d2b08
thisisbook <gamlinnia@hotmail.com>
2015-11-01 22:51:37 +0800

    done

---
+++ replaceConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+$dir = 'test/';
+
+$replaceArray = array(
+    array(
+        'beginWith' => '<div id="Copyright-icon-bg">',
+        'endWith' => 'CO., LTD. .  </div></div>',
+        'replaceTo' => '<footer></footer>'
+    )
+);
--- replaceHtml.php
+++ replaceHtml.php
@@ -1,16 +1,49 @@
 <?php
 
-$htmlContent = file_get_contents('index199b.html');
+require_once 'replaceConfig.php';
 
-$frontExpr = '<div id=top>';
-$backExpr = '<script type="text\/javascript">';
-
-if (!$backExpr) {
-    $regExpr = '/' . $frontExpr . '/';
-} else {
-    $regExpr = '/' . $frontExpr . '[^\r]+' . $backExpr . '/';
+$fileNameArray = array();
+if ($handle = opendir($dir)) {
+    while (($file = readdir($handle)) !== false){
+        if (!in_array($file, array('.', '..')) && !is_dir($dir.$file))
+            $fileNameArray[] = $file;
+    }
 }
 
-$replacedHtmlContent = preg_replace($regExpr, 'yes, i did it', $htmlContent);
 
-echo $replacedHtmlContent;
+foreach ($fileNameArray as $eachFile) {
+
+    $htmlContent = file_get_contents($dir . $eachFile);
+    if (!file_exists('backup/')) {
+        mkdir('backup/');
+    }
+    file_put_contents('backup/' .$eachFile, $htmlContent);
+
+    foreach ($replaceArray as $replaceCondition) {
+        $frontExpr = $replaceCondition['beginWith'];
+        $backExpr = escapeRegularExprString($replaceCondition['endWith']);
+        $replaceTo = $replaceCondition['replaceTo'];
+        if (!$backExpr) {
+            $regExpr = '/' . $frontExpr . '/';
+        } else {
+            $regExpr = '/' . $frontExpr . '[^\r]+' . $backExpr . '/';
+        }
+        $htmlContent = preg_replace($regExpr, $replaceTo, $htmlContent);
+
+        echo $htmlContent;
+        file_put_contents($dir . $eachFile, $htmlContent);
+    }
+}
+
+
+var_dump($fileNameArray);
+
+function escapeRegularExprString ($oriString) {
+    $stringNeedToParseArray = array(
+        '/'
+    );
+    foreach ($stringNeedToParseArray as $needToParse) {
+        $oriString = str_replace($needToParse, '\/', $oriString);
+    }
+    return $oriString;
+}
